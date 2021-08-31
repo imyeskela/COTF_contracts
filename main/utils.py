e34f1e3fbb020d4ffe2b29d60f3fb3281d911580
thisisfuckingalex <alxogloblin@gmail.com>
2021-08-31 12:25:16 +0300

    added mixin for viewing contracts

---
+++ main/utils.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+
+
+class ContractListMixin:
+    """Миксин для отображения всех контрактов"""
+
+    queryset = None
+    template_name = None
+
+    def get(self, request):
+        return render(request, self.template_name, {'contract_list': self.queryset})
