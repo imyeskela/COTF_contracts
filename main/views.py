879f7bf975d32f3176835a9c7ea1a74cd75651ec
thisisfuckingalex <alxogloblin@gmail.com>
2021-08-31 12:26:09 +0300

    view for Contract

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+from services.main_logic import get_contracts
+from main.utils import ContractListMixin
+
+
+class ContractListView(ContractListMixin, View):
+    """Отображение всех контрактов"""
+
+    queryset = get_contracts()
+    template_name = 'templates/contracts_list.html'
