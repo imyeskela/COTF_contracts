5c3deddb8a2f460ae3f0b758566a64441eaf2625
thisisfuckingalex <alxogloblin@gmail.com>
2021-08-31 11:57:38 +0300

    First

---
+++ main/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Contract(models.Model):
+    class Statuses(models.TextChoices):
+        actual = 'Актуально'
+        outdated = 'Устарело'
+
+    class Types(models.TextChoices):
+        basic = 'Основной'
+        renewal = 'Продление'
+
+    class Companies(models.TextChoices):
+        CMS = 'ООО "Комьюнити Менеджмент Солюшнз"'
+        DC = 'ООО "ДЕЛОВОЙ КЛУБ"'
+
+    name = models.CharField('Название', null=False, max_length=50, blank=True)
+    slug = models.SlugField('Url', unique=True, blank=True)
+    template_of_contract = models.FileField('Шаблон договора', null=False, upload_to='upload/')
+    amount = models.PositiveIntegerField('Сумма', null=True)
+    status = models.CharField('Статус', choices=Statuses.choices, default=Statuses.actual, max_length=50)
+    type = models.CharField('Тип', choices=Types.choices, null=False, max_length=50)
+    # branch = models.ForeignKey('Branch', on_delete=models.CASCADE, blank=True)
+    company = models.CharField('Компания', choices=Companies.choices, null=False, max_length=50)
+
+    class Meta:
+        verbose_name = 'Контракт'
+        verbose_name_plural = 'Контракты'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.name = str(self.type + ' ' + self.company)
+        self.slug = self.pk
+        super(Contract, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('contract_detail', kwargs={'contract_id': self.pk})
+
+
+class Branch(models.Model):
+    pass
