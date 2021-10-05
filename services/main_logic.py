1934f7c196484c819ce909922a1b6166a98f9357
thisisfuckingalex <alxogloblin@gmail.com>
2021-10-05 20:30:09 +0300

    upd

--- services/main_logic.py
+++ services/main_logic.py
@@ -1,27 +1,6 @@
-from io import BytesIO
 
-from django.http import HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
-import os
-from docxtpl import DocxTemplate
-from docxtpl import InlineImage
-from babel.dates import format_date
-from PIL import Image
-from docx import Document
-from docx.shared import Mm, Inches, Pt
-import qrcode
-from openpyxl import load_workbook
-from openpyxl.drawing.image import Image
-from docx2pdf import convert
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
-from django.core.mail import EmailMessage
-import base64
-import convertapi
+from django.shortcuts import get_object_or_404
 
-from cotf_contracts.settings import BASE_DIR, EMAIL_HOST_USER
-from services.num_to_text import num2text
-from main.forms import FillingQuestionnaireForm
 
 
 from main.models import ContractTemplate, Contract
