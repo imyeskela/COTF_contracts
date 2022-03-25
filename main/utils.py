bb2a5bba8597f29f7fb48772f25fab21ed6e5fe9
Michael Manvelyan <miqayel.manvelyan@gmail.com>
2022-03-25 21:18:06 +0400

    Added invalid class to error fields

--- main/utils.py
+++ main/utils.py
@@ -49,6 +49,10 @@ class ContractTemplateListAndCreateContractMixin:
                     form_contract_template.save()
                     return redirect('contract_template_list')
                 else:
+                    for field_name, field in form_contract_template.fields.items():
+                        if field_name in form_contract_template.errors:
+                            field.widget.attrs['class'] = 'invalid'
+
                     return render(
                         request,
                         self.template_name,
@@ -72,15 +76,12 @@ class ContractTemplateListAndCreateContractMixin:
             # Create client
             payload = json.loads(request.body)
 
-            print(payload)
-
             create_contract = payload.get('create_contract')
             identifier = payload.get('identifier')
             amount = payload.get('amount')
             pk = payload.get('pk')
 
             if create_contract:
-
                 contract_template = ContractTemplate.objects.get(pk=pk)
 
                 contract = Contract(
@@ -174,9 +175,9 @@ class FillingQuestionnaireMixin:
 
     def post(self, request, contract_number):
         form = self.form(request.POST, contract_pk=contract_number)
+
         if 'docx' in request.POST:
             if form.is_valid():
-
                 change_confirmation(self, request, contract_number)
                 docx_base = form_questionnaire(self, request, contract_number)
                 return render(request, self.template_name, {'docx_base': docx_base, 'form': form})
